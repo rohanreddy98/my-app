e6742fd0d08e923b0aaceca95d09e35104a352b8
rohanreddy98 <rohanreddy.venkannagari@gmail.com>
2022-01-26 20:15:53 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-import Header from './containers/Header';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 
 function App() {
   return (
     <div className="app">
-     <Header/>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
